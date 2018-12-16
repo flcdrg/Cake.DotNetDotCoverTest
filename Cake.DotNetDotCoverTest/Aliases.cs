9dc10e482304d828dacc575ebf9198e8f48dc4c7
David Gardiner <david@gardiner.net.au>
2018-12-16 20:24:44 +1030

    Add project files.

---
+++ Cake.DotNetDotCoverTest/Aliases.cs
@@ -0,0 +1,134 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+
+using System;
+
+namespace Cake.DotNetDotCoverTest
+{
+    /// <summary>
+    /// <para>Contains functionality related to <see href="https://github.com/dotnet/cli">.NET Core CLI</see>.</para>
+    /// <para>
+    /// In order to use the commands for this alias, the .Net Core CLI tools will need to be installed on the machine where
+    /// the Cake script is being executed.  See this <see href="https://www.microsoft.com/net/core">page</see> for information
+    /// on how to install.
+    /// </para>
+    /// </summary>
+    [CakeAliasCategory("DotNetCore")]
+    public static class Aliases
+    {
+        /// <summary>
+        /// Test project.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <example>
+        /// <code>
+        ///     DotNetCoreDotCoverTest();
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Test")]
+        [CakeNamespaceImport("Cake.Common.Tools.DotNetCore.Test")]
+        public static void DotNetCoreDotCoverTest(this ICakeContext context)
+        {
+            context.DotNetCoreDotCoverTest(null, null);
+        }
+
+        /// <summary>
+        /// Test project with path.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="project">The project path.</param>
+        /// <example>
+        /// <para>Specify the path to the .csproj file in the test project</para>
+        /// <code>
+        ///     DotNetCoreDotCoverTest("./test/Project.Tests/Project.Tests.csproj");
+        /// </code>
+        /// <para>You could also specify a task that runs multiple test projects.</para>
+        /// <para>Cake task:</para>
+        /// <code>
+        /// Task("Test")
+        ///     .Does(() =>
+        /// {
+        ///     var projectFiles = GetFiles("./test/**/*.csproj");
+        ///     foreach(var file in projectFiles)
+        ///     {
+        ///         DotNetCoreDotCoverTest(file.FullPath);
+        ///     }
+        /// });
+        /// </code>
+        /// <para>If your test project is using project.json, the project parameter should just be the directory path.</para>
+        /// <code>
+        ///     DotNetCoreDotCoverTest("./test/Project.Tests/");
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Test")]
+        [CakeNamespaceImport("Cake.Common.Tools.DotNetCore.Test")]
+        public static void DotNetCoreDotCoverTest(this ICakeContext context, string project)
+        {
+            context.DotNetCoreDotCoverTest(project, null);
+        }
+
+        /// <summary>
+        /// Test project with settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="project">The project path.</param>
+        /// <param name="settings">The settings.</param>
+        /// <example>
+        /// <code>
+        ///     var settings = new DotNetCoreDotCoverTestSettings
+        ///     {
+        ///         Configuration = "Release"
+        ///     };
+        ///
+        ///     DotNetCoreDotCoverTest("./test/Project.Tests/Project.Tests.csproj", settings);
+        /// </code>
+        /// <para>You could also specify a task that runs multiple test projects.</para>
+        /// <para>Cake task:</para>
+        /// <code>
+        /// Task("Test")
+        ///     .Does(() =>
+        /// {
+        ///     var settings = new DotNetCoreDotCoverTestSettings
+        ///     {
+        ///         Configuration = "Release"
+        ///     };
+        ///
+        ///     var projectFiles = GetFiles("./test/**/*.csproj");
+        ///     foreach(var file in projectFiles)
+        ///     {
+        ///         DotNetCoreDotCoverTest(file.FullPath, settings);
+        ///     }
+        /// });
+        /// </code>
+        /// <para>If your test project is using project.json, the project parameter should just be the directory path.</para>
+        /// <code>
+        ///     var settings = new DotNetCoreDotCoverTestSettings
+        ///     {
+        ///         Configuration = "Release"
+        ///     };
+        ///
+        ///     DotNetCoreDotCoverTest("./test/Project.Tests/", settings);
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Test")]
+        [CakeNamespaceImport("Cake.Common.Tools.DotNetCore.Test")]
+        public static void DotNetCoreDotCoverTest(this ICakeContext context, string project, DotNetCoreDotCoverTestSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (settings == null)
+            {
+                settings = new DotNetCoreDotCoverTestSettings();
+            }
+
+            var tester = new DotNetCoreDotCoverTester(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+            tester.Test(project, settings);
+        }
+    }
+}
---
+++ Cake.DotNetDotCoverTest/DotNetCoreDotCoverTester.cs
@@ -0,0 +1,143 @@
+﻿using Cake.Common.Tools.DotNetCore;
+using Cake.Common.Tools.DotNetCore.Test;
+using Cake.Core;
+using Cake.Core.IO;
+
+using System;
+
+namespace Cake.DotNetDotCoverTest
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DotNetCoreDotCoverTester : DotNetCoreTool<DotNetCoreTestSettings>
+    {
+        private readonly ICakeEnvironment _environment;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DotNetCoreDotCoverTester" /> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="processRunner">The process runner.</param>
+        /// <param name="tools">The tool locator.</param>
+        public DotNetCoreDotCoverTester(
+            IFileSystem fileSystem,
+            ICakeEnvironment environment,
+            IProcessRunner processRunner,
+            Cake.Core.Tooling.IToolLocator tools) : base(fileSystem, environment, processRunner, tools)
+        {
+            _environment = environment;
+        }
+
+        /// <summary>
+        /// Tests the project using the specified path with arguments and settings.
+        /// </summary>
+        /// <param name="project">The target project path.</param>
+        /// <param name="settings">The settings.</param>
+        public void Test(string project, DotNetCoreTestSettings settings)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            RunCommand(settings, GetArguments(project, settings));
+        }
+
+        private ProcessArgumentBuilder GetArguments(string project, DotNetCoreTestSettings settings)
+        {
+            var builder = CreateArgumentBuilder(settings);
+
+            builder.Append("dotcover");
+            builder.Append("test");
+
+            // Specific path?
+            if (project != null)
+            {
+                builder.AppendQuoted(project);
+            }
+
+            // Settings
+            if (settings.Settings != null)
+            {
+                builder.Append("--settings");
+                builder.AppendQuoted(settings.Settings.MakeAbsolute(_environment).FullPath);
+            }
+
+            // Filter
+            if (!string.IsNullOrWhiteSpace(settings.Filter))
+            {
+                builder.Append("--filter");
+                builder.AppendQuoted(settings.Filter);
+            }
+
+            // Settings
+            if (settings.TestAdapterPath != null)
+            {
+                builder.Append("--test-adapter-path");
+                builder.AppendQuoted(settings.TestAdapterPath.MakeAbsolute(_environment).FullPath);
+            }
+
+            // Filter
+            if (!string.IsNullOrWhiteSpace(settings.Logger))
+            {
+                builder.Append("--logger");
+                builder.AppendQuoted(settings.Logger);
+            }
+
+            // Output directory
+            if (settings.OutputDirectory != null)
+            {
+                builder.Append("--output");
+                builder.AppendQuoted(settings.OutputDirectory.MakeAbsolute(_environment).FullPath);
+            }
+
+            // Frameworks
+            if (!string.IsNullOrEmpty(settings.Framework))
+            {
+                builder.Append("--framework");
+                builder.Append(settings.Framework);
+            }
+
+            // Configuration
+            if (!string.IsNullOrEmpty(settings.Configuration))
+            {
+                builder.Append("--configuration");
+                builder.Append(settings.Configuration);
+            }
+
+            // Output directory
+            if (settings.DiagnosticFile != null)
+            {
+                builder.Append("--diag");
+                builder.AppendQuoted(settings.DiagnosticFile.MakeAbsolute(_environment).FullPath);
+            }
+
+            // No Build
+            if (settings.NoBuild)
+            {
+                builder.Append("--no-build");
+            }
+
+            // No Restore
+            if (settings.NoRestore)
+            {
+                builder.Append("--no-restore");
+            }
+
+            if (settings.ResultsDirectory != null)
+            {
+                builder.Append("--results-directory");
+                builder.AppendQuoted(settings.ResultsDirectory.MakeAbsolute(_environment).FullPath);
+            }
+
+            if (settings.VSTestReportPath != null)
+            {
+                builder.AppendSwitchQuoted($"--logger trx;LogFileName", "=", settings.VSTestReportPath.MakeAbsolute(_environment).FullPath);
+            }
+
+            return builder;
+        }
+    }
+}
